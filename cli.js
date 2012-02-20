54944be6e32313665621188b7a55ac460407aa8c
dashersw <armagan@amcalar.com>
2012-02-20 15:56:54 +0200

    Introduced cli.js for command line operations.

---
+++ cli.js
@@ -0,0 +1,45 @@
+// Copyright 2011 Armagan Amcalar. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview
+ */
+
+
+require('nclosure').nclosure();
+
+goog.provide('geneJS.cli');
+goog.require('geneJS.Registry');
+goog.require('geneJS.LanguageLoader');
+goog.require('geneJS.Generator');
+require('fs');
+
+
+
+geneJS.cli.process = function(language, inputFolder, outputFolder) {
+	var languageLoader = new geneJS.LanguageLoader();
+	languageLoader.load();
+
+	var registry = new geneJS.Registry(inputFolder);
+
+	var generator = new geneJS.Generator(registry.getInputs());
+
+	var outputs = generator.generate(languageLoader.languages[language]);r
+	goog.object.forEach(outputs, function(output, key) {
+		fs.writeFile(outputFolder + '/' + key + '.js', output);
+	});
+};
+
+module.exports = geneJS.cli;
+
